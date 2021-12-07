c0c32e9e4a280c521e33d1e98d81057efaccb08a
Shahwar Ali <Shahwaralirajpoot12@gmail.com>
2021-12-08 02:57:52 +0500

    Dom Object

---
+++ app.js
@@ -0,0 +1,65 @@
+// alert("Pakistan Zindabad")
+
+// Chapter No 58 to 67
+
+// Q1
+
+// var mainContent = document.getElementById("main-content")
+
+// var childNodes = mainContent.children;
+// console.log(childNodes)
+
+// var render = document.getElementsByClassName("render")
+// console.log(render)
+
+// var firstName = document.getElementById("first-name")
+// firstName.value = "Alex";
+// console.log(firstName)
+
+// var lastName = document.getElementById("last-name")
+// lastName.value = "Bank";
+// console.log(lastName)
+
+// var email = document.getElementById("email")
+// email.value = "Alexbank@example.com";
+// console.log(email)
+
+
+// 2
+
+// var formContent = document.getElementById("form-content");
+// formContent.nodeType;
+// console.log(formContent.nodeType)
+
+// var lastName = document.getElementById("lastName");
+// console.log(lastName.nodeType, lastName.children)
+
+
+// var child = document.getElementById("lastName")
+// console.log(child.childNodes)
+// child.innerHTML = "Father Name";
+// console.log(child)
+
+
+// var mainContent = document.getElementById("main-content")
+// console.log(mainContent)
+// console.log(mainContent.firstElementChild)
+// console.log(mainContent.lastElementChild)
+
+
+// var lastName = document.getElementById("lastName") 
+// console.log(lastName)
+// console.log(lastName.previousElementSibling)
+// console.log(lastName.nextElementSibling)
+
+
+// var email = document.getElementById("email") 
+// console.log(email)
+// console.log(email.parentNode)
+// console.log(email.nodeType)
+
+
+
+
+
+
